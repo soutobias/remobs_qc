af7212933fe875e86bc5ac8bba85a501db8fcdd8
Felippe Galdino <ocfgaldino@gmail.com>
2021-03-16 09:28:40 -0300

    Bmo script alert

--- alerts/status_position_bmo.py
+++ alerts/status_position_bmo.py
@@ -32,7 +32,7 @@ bmo_now = [float(bmo_now['lon']), float(bmo_now['lat'])]
 
 
 
-pts_bmo = conn.last_positions('BMO', 2, 1600)
+pts_bmo = conn.last_positions('BMO', 2, 2100)
 pts_lat_bmo = (pts_bmo['lat'].values).astype(np.float)
 pts_lon_bmo = (pts_bmo['lon'].values).astype(np.float)
 
@@ -42,7 +42,7 @@ coords_bmo = [[pts_lon_bmo[p],pts_lat_bmo[p]] for p in range(len(pts_lat_bmo))]
 safe_radius = radius_buoy(2164, 2100,300,27.5,15)
 center_lon, center_lat = find_centroid(pts_lon_bmo, pts_lat_bmo)
 
-new_center_lon , new_center_lat = find_centroid(outer_points_lon, outer_points_lat)
+#new_center_lon , new_center_lat = find_centroid(outer_points_lon, outer_points_lat)
 
 #lon_in, lat_in = find_outer_points(pts_lon_bmo, pts_lat_bmo, center_lon, center_lat)
 
@@ -72,8 +72,8 @@ ax.set(facecolor = "#5ACEFF")
 ax.add_feature(cfeature.LAND)
 ax.add_feature(cfeature.COASTLINE)
 
-ax.set_xlim(float(center_lon)-0.03, float(center_lon)+0.03)
-ax.set_ylim(float(center_lat)-0.03, float(center_lat)+0.03)
+ax.set_xlim(float(center_lon)-0.15, float(center_lon)+0.15)
+ax.set_ylim(float(center_lat)-0.15, float(center_lat)+0.15)
 bmo_points = ax.plot(pts_lon_bmo,pts_lat_bmo, c='r', marker='o' ,label = 'BMO', alpha=0.2)
 
 ## BORDER POINTS
