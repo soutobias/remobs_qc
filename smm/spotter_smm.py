803776135cd580837e237003fd1713dd4c3eab4d
Felippe Galdino <ocfgaldino@gmail.com>
2021-12-08 13:59:07 -0300

    New Spotters buoys added - Operantar

--- smm/spotter_smm.py
+++ smm/spotter_smm.py
@@ -7,8 +7,6 @@ sys.path.append(home_path)
 import bd_function as bd
 import pandas as pd
 
-from user_config import EMAIL_SPOTTER_ABROLHOS_FILE, EMAIL_SPOTTER_MEXILHAO_FILE
-
 
 def get_synoptic_data(df):
 
@@ -75,8 +73,10 @@ def get_full_hour(df):
 
 
 # Abrolhos Buoy
+print("Connecting to database...")
 conn = bd.conn_qc_db('PRI')
 
+print("Abrolhos buoy...")
 df_spotter_qc_abrolhos = bd.get_data_spotter(conn=conn, buoy_id=20, table='data_buoys', start_date="2021-06-21",last_date=None, interval_hour='ALL')
 df_spotter_qc_abrolhos.sort_values(by = 'date_time', inplace = True)
 
@@ -105,8 +105,112 @@ df_spotter_abrolhos = get_synoptic_data(df_spotter_abrolhos)
 
 
 
-bd.spotter_txt(df_spotter_abrolhos, EMAIL_SPOTTER_ABROLHOS_FILE)
+bd.spotter_txt(df_spotter_abrolhos, 'dados_spotter_abrolhos.txt')
+print("Abrolhos buoy file ready.")
 
+# Potter Buoy
+
+print("Potter buoy ...")
+df_spotter_qc_potter = bd.get_data_spotter(conn=conn, buoy_id=25, table='data_buoys', start_date="2021-12-08",last_date=None, interval_hour='ALL')
+df_spotter_qc_potter.sort_values(by = 'date_time', inplace = True)
+
+
+
+
+df_spotter_potter = df_spotter_qc_potter[['date_time', 'lat', 'lon', 'wspd', 'wdir','sst',
+							'swvht1','tp1', 'wvdir1','wvspread1', 'pk_dir',
+							'pk_wvspread','mean_tp']].copy()
+
+
+
+
+df_spotter_potter.rename(columns = {'date_time':'Datetime',
+							 'sst':'wtmp',
+							 'swvht1': 'wvht',
+							 'tp1': 'dpd',
+							 'mean_tp':'mean_dpd',
+							 'wvdir1':'mwd',
+							 'pk_dir':'peak_mwd',
+							 'wvspread1':'spred',
+							 'pk_wvspread':'peak_spred'}, inplace = True)
+
+df_spotter_potter = get_full_hour(df_spotter_potter)
+df_spotter_potter = get_synoptic_data(df_spotter_potter)
+
+
+
+bd.spotter_txt(df_spotter_potter, 'dados_spotter_potter.txt')
+print("Potter buoy file ready.")
+
+
+# Pinguim Buoy
+
+print("Pinguim buoy...")
+df_spotter_qc_pinguim = bd.get_data_spotter(conn=conn, buoy_id=26, table='data_buoys', start_date="2021-12-04",last_date=None, interval_hour='ALL')
+df_spotter_qc_pinguim.sort_values(by = 'date_time', inplace = True)
+
+
+
+
+df_spotter_pinguim = df_spotter_qc_pinguim[['date_time', 'lat', 'lon', 'wspd', 'wdir','sst',
+							'swvht1','tp1', 'wvdir1','wvspread1', 'pk_dir',
+							'pk_wvspread','mean_tp']].copy()
+
+
+
+
+df_spotter_pinguim.rename(columns = {'date_time':'Datetime',
+							 'sst':'wtmp',
+							 'swvht1': 'wvht',
+							 'tp1': 'dpd',
+							 'mean_tp':'mean_dpd',
+							 'wvdir1':'mwd',
+							 'pk_dir':'peak_mwd',
+							 'wvspread1':'spred',
+							 'pk_wvspread':'peak_spred'}, inplace = True)
+
+df_spotter_pinguim = get_full_hour(df_spotter_pinguim)
+df_spotter_pinguim = get_synoptic_data(df_spotter_pinguim)
+
+
+
+bd.spotter_txt(df_spotter_pinguim, 'dados_spotter_pinguim.txt')
+print("Pinguim buoy file ready.")
+
+
+# Keller Buoy
+
+print("Keller buoy...")
+df_spotter_qc_keller = bd.get_data_spotter(conn=conn, buoy_id=24, table='data_buoys', start_date="2021-12-04",last_date=None, interval_hour='ALL')
+df_spotter_qc_keller.sort_values(by = 'date_time', inplace = True)
+
+
+
+
+df_spotter_keller = df_spotter_qc_keller[['date_time', 'lat', 'lon', 'wspd', 'wdir','sst',
+							'swvht1','tp1', 'wvdir1','wvspread1', 'pk_dir',
+							'pk_wvspread','mean_tp']].copy()
+
+
+
+
+df_spotter_keller.rename(columns = {'date_time':'Datetime',
+							 'sst':'wtmp',
+							 'swvht1': 'wvht',
+							 'tp1': 'dpd',
+							 'mean_tp':'mean_dpd',
+							 'wvdir1':'mwd',
+							 'pk_dir':'peak_mwd',
+							 'wvspread1':'spred',
+							 'pk_wvspread':'peak_spred'}, inplace = True)
+
+df_spotter_keller = get_full_hour(df_spotter_keller)
+df_spotter_keller = get_synoptic_data(df_spotter_keller)
+
+
+
+bd.spotter_txt(df_spotter_keller, 'dados_spotter_keller.txt')
+print("Keller buoy file ready.")
 
 # # Mexilao Buoy
 # conn = bd.conn_qc_db('PRI')
